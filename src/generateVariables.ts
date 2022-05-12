a6e4cf5a68cc1a98fe7a34ad7e43e9dc1af090f2
ro <roc0ast3r@gmail.com>
2022-05-11 19:16:35 -0700

    Create generateVariables.ts

---
+++ src/generateVariables.ts
@@ -0,0 +1,21 @@
+const generateVariables = () => {
+    const bannedChars = ["{", "}", ".", ","]
+
+    const variables = {}
+    const variablesEl = document.querySelectorAll("set")
+    for (let i = 0; i < variablesEl.length; i++) {
+        const variable = variablesEl[i]
+        const variableName = variable.getAttribute("name")
+        const variableContent = variable.innerHTML
+
+        for (let i = 0; i < bannedChars.length; i++) {
+            if (variableName.includes(bannedChars[i])) return exmlErr(`Variable name ${variableName} uses banned character (${bannedChars[i]}).`)
+        }
+
+        variables[variableName] = variableContent
+        variable.remove()
+
+        const variableRegex = RegExp(`(?<!<if exists="){{${variableName}}}`, "g")
+        document.body.innerHTML = document.body.innerHTML.replace(variableRegex, variableContent)
+    }
+}
