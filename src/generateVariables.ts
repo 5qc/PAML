81f9ab772f2424a14582b14b3b51c320480b40d2
ro <roc0ast3r@gmail.com>
2022-05-12 13:04:26 -0700

    Update generateVariables.ts

--- src/generateVariables.ts
+++ src/generateVariables.ts
@@ -6,6 +6,8 @@ const generateVariables = () => {
         const variableName = variable.getAttribute("name")
         const variableContent = variable.innerHTML
 
+        console.log(variable.parentElement.tagName)
+
         for (let i = 0; i < bannedChars.length; i++) {
             if (variableName.includes(bannedChars[i])) return pamlErr(`Variable name ${variableName} uses banned character (${bannedChars[i]}).`)
         }
